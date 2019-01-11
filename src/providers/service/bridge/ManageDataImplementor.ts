127c7d1d1746890f160f0b816b5372fbccb42c06
Andrea <valentiandrea1993@gmail.com>
2019-01-11 18:40:41 +0100

    Remove Bridge

--- src/providers/service/bridge/ManageDataImplementor.ts
+++
@@ -1,6 +0,0 @@
-interface ManageDataImplementor{
-    getDocument(collectionObj: string):any;
-    addDcoument(collectionObj: string,dataObj: any):any;
-    editDocument(collectionObj: string,docID: string,dataObj: any):any;
-    deleteDocument(collectionObj: string,docID: string):any;
-}
