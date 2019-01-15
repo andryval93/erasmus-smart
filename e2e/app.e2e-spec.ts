0f62b1df68c16d08c6e4d0b433752d7e0179768d
Unknown <a.massaro196@gmail.com>
2019-01-15 11:44:30 +0100

    fix

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,7 +9,7 @@ describe('App', () => {
   });
 
 
-/*
+
   describe('default screen', () => {
     beforeEach(() => {
       page.navigateTo('/');
@@ -68,10 +68,10 @@ describe('App', () => {
         return e.isDisplayed();
       }, 2000);
     });
-  })*/
+  })
 
 
-
+/*
   describe('registration error wrong mail and pass', () => {
     beforeEach(() => {
       page.navigateTo('/');
@@ -88,24 +88,33 @@ describe('App', () => {
       let place = element(by.id('place'));
       let cf = element(by.id('cf'));
       let privacy = element(by.id('privacy'));
+      let done: boolean=false;
+      let first: boolean=true;
+      //browser.sleep(100);
+      browser.waitForAngular();
+      browser.wait(function(){
+        //if(first&&done){
+      browser.actions().click(name).sendKeys("Andrea").perform();
+      browser.actions().click(surname).sendKeys("Massaro").perform();
+      //first=false;}
+      //if(!done){
+      browser.actions().click(date).sendKeys("10121996").perform();
+      //done=true;
+      browser.actions().click(place).sendKeys("Potenza").perform();
+      browser.actions().click(cf).sendKeys("MSSNDR96T10G942I").perform();
       browser.actions().click(email).sendKeys("a.massaro19@gmail.com").perform();
       browser.actions().click(password).sendKeys("123456").perform();
       browser.actions().click(check_p).sendKeys("123456").perform();
-      browser.actions().click(name).sendKeys("Andrea").perform();
-      browser.actions().click(surname).sendKeys("Massaro").perform();
-      browser.actions().click(date).sendKeys("10121996").perform();
-      browser.actions().click(place).sendKeys("Potenza").perform();
-      browser.actions().click(cf).sendKeys("MSSNDR96T10G942I").perform();
-      privacy.click();
-      browser.wait(function () {
+      privacy.click();}/*},10000)
+      /*browser.wait(function () {
         //return e.isDisplayed();
       }, 20000);
     });
-  })
+  })*/
 
 
 
-/*
+
   describe('logged soccessfully', () => {
     beforeEach(() => {
       page.navigateTo('/');
@@ -123,7 +132,7 @@ describe('App', () => {
       }
         , 10000)
     });
-  })*/
+  })
 
 
 
