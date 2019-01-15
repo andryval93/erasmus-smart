fd0079054d47d426b88c3e7d6a0defdbcbee4432
Unknown <buonincontrigio@gmail.com>
2019-01-15 11:51:50 +0100

    NewsService unit updated + first system test QeA

--- src/app/news.spec.ts
+++ src/app/news.spec.ts
@@ -1,6 +1,7 @@
 import { NewsServiceProvider } from '../providers/service/newsService'
 import { TestBed, async, inject } from '@angular/core/testing';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { textChangeRangeIsUnchanged } from 'typescript';
 
 describe('Service: newsService', () => {
     beforeEach(async(() => {
@@ -15,7 +16,7 @@ describe('Service: newsService', () => {
         })
     }));
 
-    it('should call fetchData from apiService', function (done) {
+    it('should call getNews from NewsService', function (done) {
         let service :NewsServiceProvider =  new NewsServiceProvider();
 
         spyOn(service, 'getNews').and.returnValue(Promise.resolve([{
@@ -28,7 +29,54 @@ describe('Service: newsService', () => {
         service.getNews('News').then((data) => {
             console.log("News data",data)
             expect(data[0].title).toEqual("NEWS dell'admin 18:07 :)")
+            expect(service.getNews).toHaveBeenCalledWith("News")
             done();
         })
     });
+
+    it('should not call getNews from NewsService', function (done) {
+        let service: NewsServiceProvider =  new NewsServiceProvider();
+
+        spyOn(service, 'getNews').and.returnValue(Promise.reject([{
+            errore: "fallito"
+        }]));
+
+        service.getNews('News').then().catch((error) => {
+            expect(error[0].errore).toEqual("fallito");
+            done();
+        })
+    });
+
+    it('should call insertNews NewsService', function (done) {
+        let service: NewsServiceProvider =  new NewsServiceProvider();
+
+        spyOn(service, 'insertNews').and.returnValue(Promise.resolve());
+        service.insertNews("News", "newNews", {title: "title", content: "contenuto", date: new Date()});
+
+        expect(service.insertNews).toHaveBeenCalled();
+        done();
+    });
+
+    it('should not call insertNews NewsService', function (done) {
+        let service: NewsServiceProvider =  new NewsServiceProvider();
+
+        spyOn(service, 'insertNews').and.returnValue(Promise.resolve());
+        
+        expect(service.insertNews).not.toHaveBeenCalled();
+        done();
+    });
+
+    it('should not insert news', function (done) {
+        let service: NewsServiceProvider =  new NewsServiceProvider();
+
+        spyOn(service, 'insertNews').and.returnValue(Promise.reject([{
+            errore: "fallito"
+        }]));
+
+        service.insertNews('News', "", {}).then().catch((error) => {
+            expect(error[0].errore).toEqual("fallito");
+            done();
+        })
+    });
+
 });
