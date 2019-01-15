e4d0efd901de27f91d86049b0275a6996a1367a4
Unknown <a.massaro196@gmail.com>
2019-01-15 09:59:06 +0100

    test login done

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Page } from './app.po';
-import { browser, by, element } from 'protractor';
+import { browser, by, element, until } from 'protractor';
 
 describe('App', () => {
   let page: Page;
@@ -10,48 +10,87 @@ describe('App', () => {
 
 
   
-  describe('default screen', () => {
-    beforeEach(() => {
-      page.navigateTo('/');
-    });
-
-    it('loggedInfo is present', () => {
-      element(by.id('loggedInfo')).isPresent()
-    });
-  })
-
-
-
-  describe('default screen', () => {
-    beforeEach(() => {
-      page.navigateTo('/');
-    });
-
-    it('loggedInfo is Displayed', () => {
-      element(by.id('ButtonMenu')).click();
-      browser.wait(function () {
-        return element(by.id('MenuList')).isDisplayed();
-      }, 2000);
-    });
-  })
-
-
-
-
-    describe('logged soccessfully', () => {
+    describe('default screen', () => {
       beforeEach(() => {
         page.navigateTo('/');
       });
-
-      it('news is Displayed', () => {
-        let e = element(by.id('email'));
-        let p = element(by.id('pass'))
-        browser.actions().click(e).sendKeys("a.massaro196@gmail.com").perform();
-        browser.actions().click(p).sendKeys("123456").perform();
-        element(by.id('loginButton')).click();
+  
+      it('loggedInfo is present', () => {
+        element(by.id('loggedInfo')).isPresent()
+      });
+    })
+  
+  
+  
+    describe('default screen', () => {
+      beforeEach(() => {
+        page.navigateTo('/');
+      });
+  
+      it('loggedInfo is Displayed', () => {
+        element(by.id('ButtonMenu')).click();
         browser.wait(function () {
-          //element(by.id('email')).sendKeys('a.massaro5555');
+          return element(by.id('MenuList')).isDisplayed();
         }, 2000);
       });
     })
-  });
+  
+  
+    describe('logged not soccessfully no pass', () => {
+      beforeEach(() => {
+        page.navigateTo('/');
+      });
+  
+      it('news is not Displayed', () => {
+          let e = element(by.id('email'));
+          let p = element(by.id('pass'));
+          browser.actions().click(e).sendKeys("a.massaro196@gmail.com").perform();
+          element(by.id('loginButton')).click();
+        browser.wait(function () {
+          return e.isDisplayed();
+        }, 2000);
+      });
+    })
+
+
+    describe('logged not soccessfully wrong mail and pass', () => {
+      beforeEach(() => {
+        page.navigateTo('/');
+      });
+  
+      it('news is not Displayed', () => {
+          let e = element(by.id('email'));
+          let p = element(by.id('pass'));
+          browser.actions().click(e).sendKeys("a.massaro19@gmail.com").perform();
+          browser.actions().click(p).sendKeys("1234").perform();
+          element(by.id('loginButton')).click();
+        browser.wait(function () {
+          return e.isDisplayed();
+        }, 2000);
+      });
+    })
+
+
+
+
+  describe('logged soccessfully', () => {
+    beforeEach(() => {
+      page.navigateTo('/');
+    });
+
+    it('news is Displayed', () => {
+      let e = element(by.id('email'));
+      let p = element(by.id('pass'))
+      browser.actions().click(e).sendKeys("a.massaro5@studenti.unisa.it").perform();
+      browser.actions().click(p).sendKeys("123456").perform();
+      element(by.id('loginButton')).click();
+      browser.ignoreSynchronization = true;
+      browser.wait(function () {
+        return element(by.buttonText('Aggiungi News')).isPresent();
+      }
+        , 10000)
+    });
+  })
+
+
+});
